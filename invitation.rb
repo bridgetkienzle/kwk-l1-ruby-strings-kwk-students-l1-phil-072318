86d55e2b8ab2a208e38fd2bb3ca9c8317739d27d
Bridget Kienzle <bridgetkien@gmail.com>
2018-07-23 20:17:29 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,3 +2,10 @@
 
 # Try starting out with puts'ing a string.
 
+puts "Dear #{guest_name},
+
+You are cordially invited to  Ever on October 31 at 6pm. Please RSVP no later than October 30.
+
+Sincerely,
+
+Harry Potter"
